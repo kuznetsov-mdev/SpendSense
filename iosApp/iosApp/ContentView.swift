2dbcbe506c90b2ba26f5c6603e4ae6ff8fff8a19
gkuznetsov <kuznetsov.mdev@gmail.com>
2025-05-02 22:29:21 +0500

    SpSe-iOS: add iOS project

---
+++ iosApp/iosApp/ContentView.swift
@@ -0,0 +1,16 @@
+import SwiftUI
+import shared
+
+struct ContentView: View {
+    var body: some View {
+       ComposeView()
+    }
+}
+
+struct ComposeView : UIViewControllerRepresentable {
+    func updateUIViewController(_ uiViewController: UIViewControllerType, context: Context) {}
+    
+    func makeUIViewController(context: Context) -> some UIViewController {
+        RootViewKt.MainViewController()
+    }
+}
---
+++ iosApp/iosApp/iosAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  iosAppApp.swift
+//  iosApp
+//
+//  Created by Gleb Kuznetsov on 02.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct iosAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
