17eccd5095301d23d4b37309b0810c4740e36aa9
gkuznetsov <kuznetsov.mdev@gmail.com>
2025-05-06 08:55:31 +0500

    SpSe-MVVM: add theme switching to SettingsScreen

--- shared/src/commonMain/kotlin/ru/sbx/spend_sense/presentation/root/RootScreen.kt
+++ shared/src/commonMain/kotlin/ru/sbx/spend_sense/presentation/root/RootScreen.kt
@@ -1,8 +1,8 @@
 package ru.sbx.spend_sense.presentation.root
 
 import androidx.compose.runtime.Composable
-import ru.sbx.spend_sense.presentation.settings.SettingsScreen
 import ru.sbx.spend_sense.presentation.settings.SettingsViewModel
+import ru.sbx.spend_sense.presentation.settings.compose.SettingsScreen
 
 @Composable
 fun RootScreen() {
