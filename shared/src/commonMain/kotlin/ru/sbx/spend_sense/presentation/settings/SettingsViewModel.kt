e2b1e68456a02732c58495c2133a026023aaada0
gkuznetsov <kuznetsov.mdev@gmail.com>
2025-05-06 08:48:10 +0500

    SpSe-MVVM: add theme switching to settings view model

--- shared/src/commonMain/kotlin/ru/sbx/spend_sense/presentation/settings/SettingsViewModel.kt
+++ shared/src/commonMain/kotlin/ru/sbx/spend_sense/presentation/settings/SettingsViewModel.kt
@@ -1,12 +1,20 @@
 package ru.sbx.spend_sense.presentation.settings
 
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
 import ru.sbx.spend_sense.platform.DeviceInfo
 import ru.sbx.spend_sense.presentation.base.BaseViewModel
 import ru.sbx.spend_sense.presentation.settings.SettingsContract.State
+import ru.sbx.spend_sense.storage.SettingsManager
 
 class SettingsViewModel : BaseViewModel<State, Nothing>() {
 
     init {
+
+        SettingsManager.isDarkThemFlow.onEach {
+            updateState { copy(isDarkTheme = it) }
+        }.launchIn(viewModelScope)
+
         val deviceInfo = DeviceInfo()
 
         updateState {
@@ -15,4 +23,8 @@ class SettingsViewModel : BaseViewModel<State, Nothing>() {
     }
 
     override fun initialState() = State.NONE
+
+    fun switchTheme(isDark: Boolean) {
+        SettingsManager.isDarkTheme = isDark
+    }
 }
