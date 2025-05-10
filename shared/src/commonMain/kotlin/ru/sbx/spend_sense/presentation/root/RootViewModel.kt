e8c01905fc3ff5742a76ab9a726e6067933a2d7f
gkuznetsov <kuznetsov.mdev@gmail.com>
2025-05-10 11:22:20 +0500

    SpSe-Theme: subscribe view model to theme changing

--- shared/src/commonMain/kotlin/ru/sbx/spend_sense/presentation/root/RootViewModel.kt
+++ shared/src/commonMain/kotlin/ru/sbx/spend_sense/presentation/root/RootViewModel.kt
@@ -1,8 +1,17 @@
 package ru.sbx.spend_sense.presentation.root
 
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
 import ru.sbx.spend_sense.presentation.base.BaseViewModel
 import ru.sbx.spend_sense.presentation.root.RootContract.State
+import ru.sbx.spend_sense.storage.SettingsManager
 
 class RootViewModel : BaseViewModel<State, Nothing>() {
     override fun initialState() = State.NONE
+
+    init {
+        SettingsManager.isDarkThemFlow.onEach { isDark ->
+            updateState { copy(isDarkTheme = isDark) }
+        }.launchIn(viewModelScope)
+    }
 }
