3c25207db74d3589830385e43817422751cf0ef7
gkuznetsov <kuznetsov.mdev@gmail.com>
2025-05-13 09:32:45 +0500

    SpSe-resources: add BottomBar to RootScreen with simple navigation

--- shared/src/commonMain/kotlin/ru/sbx/spend_sense/presentation/categories/CategoriesScreen.kt
+++ shared/src/commonMain/kotlin/ru/sbx/spend_sense/presentation/categories/CategoriesScreen.kt
@@ -1,9 +1,12 @@
 package ru.sbx.spend_sense.presentation.categories
 
+import androidx.compose.foundation.layout.BoxScope
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 
 @Composable
-fun CategoriesScreen() {
-    Text("Categories")
+fun BoxScope.CategoriesScreen() {
+    Text("Categories", modifier = Modifier.align(Alignment.Center))
 }
