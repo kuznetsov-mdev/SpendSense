6e6255b72028fd305ff2d7c6c6375506f9dfeb83
gkuznetsov <kuznetsov.mdev@gmail.com>
2025-05-20 20:25:38 +0500

    SpSe-multiplatform-settings: add wrapper for Settings in SettingsManager

--- shared/src/commonMain/kotlin/ru/sbx/spend_sense/storage/SettingsManager.kt
+++ shared/src/commonMain/kotlin/ru/sbx/spend_sense/storage/SettingsManager.kt
@@ -1,15 +1,27 @@
 package ru.sbx.spend_sense.storage
 
+import com.russhwolf.settings.Settings
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
 
+expect class AppSettings constructor() {
+    val settings: Settings
+}
+
 object SettingsManager {
-    var isDarkTheme: Boolean = false
-        set(value) {
-            _isDarkThemeFlow.update { value }
-            field = value
-        }
+    private const val THEME_KEY = "THEME_KEY"
+
+    val appSettings: AppSettings = AppSettings()
+
     private val _isDarkThemeFlow = MutableStateFlow(isDarkTheme)
     val isDarkThemFlow = _isDarkThemeFlow.asStateFlow()
+
+    var isDarkTheme: Boolean
+        set(value) {
+            _isDarkThemeFlow.update { value }
+            appSettings.settings.putBoolean(THEME_KEY, value)
+        }
+        get() = appSettings.settings.getBoolean(THEME_KEY, true)
+
 }
