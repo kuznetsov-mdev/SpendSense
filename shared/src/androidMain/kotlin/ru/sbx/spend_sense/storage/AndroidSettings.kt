dbd06e0f280038a5cf552a468e143940d1f3a44d
gkuznetsov <kuznetsov.mdev@gmail.com>
2025-05-20 20:48:04 +0500

    SpSe-multiplatform-settings: add actual realization for other platforms

--- shared/src/androidMain/kotlin/ru/sbx/spend_sense/storage/AndroidSettings.kt
+++ shared/src/androidMain/kotlin/ru/sbx/spend_sense/storage/AndroidSettings.kt
@@ -5,7 +5,7 @@ import com.russhwolf.settings.Settings
 import com.russhwolf.settings.SharedPreferencesSettings
 import ru.sbx.spend_sense.App
 
-actual class AppSettings actual constructor() {
+actual class AppSettings {
     actual val settings: Settings
         get() = SharedPreferencesSettings(
             App.instance.getSharedPreferences("AppSettings", Context.MODE_PRIVATE)
