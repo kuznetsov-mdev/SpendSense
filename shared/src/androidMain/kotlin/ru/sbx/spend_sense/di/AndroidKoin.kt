52efc618b2cfc348544494d48659f32ab392a4f3
gkuznetsov <kuznetsov.mdev@gmail.com>
2025-05-23 09:12:39 +0500

    SpSe-DI-Koin: fix NoBeanDefFoundException: No definition found for class:'com.russhwolf.settings.Settings'

--- shared/src/androidMain/kotlin/ru/sbx/spend_sense/di/AndroidKoin.kt
+++ shared/src/androidMain/kotlin/ru/sbx/spend_sense/di/AndroidKoin.kt
@@ -1,10 +1,12 @@
 package ru.sbx.spend_sense.di
 
 import android.content.Context
+import com.russhwolf.settings.Settings
 import com.russhwolf.settings.SharedPreferencesSettings
+import org.koin.dsl.bind
 import org.koin.dsl.module
 
 actual val platformModule = module {
     single { get<Context>().getSharedPreferences("appSettings", Context.MODE_PRIVATE) }
-    single { SharedPreferencesSettings(get()) }
+    single { SharedPreferencesSettings(get()) } bind Settings::class
 }
