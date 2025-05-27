0d5968d4f3f28ff00e8e8d5abe2f1353b5584f3b
gkuznetsov <kuznetsov.mdev@gmail.com>
2025-05-27 09:27:01 +0500

    SpSe-DI-Koin add Koin initialization for JVM

--- desktop/src/desktopMain/kotlin/Main.kt
+++ desktop/src/desktopMain/kotlin/Main.kt
@@ -4,10 +4,13 @@ import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 import androidx.compose.ui.window.rememberWindowState
 import ru.sbx.spend_sense.di.getKoinInstance
+import ru.sbx.spend_sense.di.initKoin
 import ru.sbx.spend_sense.presentation.root.RootViewModel
 import ru.sbx.spend_sense.presentation.root.compose.RootScreen
 
 fun main() {
+
+    initKoin()
 
     application {
         val state = rememberWindowState().apply { size = DpSize(1200.dp, 1200.dp) }
