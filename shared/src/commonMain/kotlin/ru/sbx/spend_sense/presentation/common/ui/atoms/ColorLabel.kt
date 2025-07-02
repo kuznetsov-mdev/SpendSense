07fb2913443ea6ce8c07ce1da52bde038d6a782e
gkuznetsov <kuznetsov.mdev@gmail.com>
2025-07-02 16:02:26 +0500

    SpSe-categories-ui: fix getting color in ColorLabel

--- shared/src/commonMain/kotlin/ru/sbx/spend_sense/presentation/common/ui/atoms/ColorLabel.kt
+++ shared/src/commonMain/kotlin/ru/sbx/spend_sense/presentation/common/ui/atoms/ColorLabel.kt
@@ -10,13 +10,15 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import ru.sbx.spend_sense.extentions.fromHex
+import ru.sbx.spend_sense.presentation.common.ui.theme.AppThemeProvider
 
 @Composable
 fun ColorLabel(colorHex: String) {
+    val color = runCatching { Color.fromHex(colorHex) }.getOrNull() ?: AppThemeProvider.colors.accent
     Box(
         modifier = Modifier
             .size(40.dp)
-            .background(Color.fromHex(colorHex).copy(alpha = 0.8f), RoundedCornerShape(8.dp))
-            .border(2.dp, Color.fromHex(colorHex), RoundedCornerShape(8.dp))
+            .background(color.copy(alpha = 0.8f), RoundedCornerShape(8.dp))
+            .border(2.dp, color, RoundedCornerShape(8.dp))
     )
 }
