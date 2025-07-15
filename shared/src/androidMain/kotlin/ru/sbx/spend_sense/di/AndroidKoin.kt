91175391db7d2d30453001ecb39a88ec6e17ab3e
gkuznetsov <kuznetsov.mdev@gmail.com>
2025-07-15 09:02:02 +0500

    SpSe-db: provide SqlDriver

--- shared/src/androidMain/kotlin/ru/sbx/spend_sense/di/AndroidKoin.kt
+++ shared/src/androidMain/kotlin/ru/sbx/spend_sense/di/AndroidKoin.kt
@@ -1,12 +1,16 @@
 package ru.sbx.spend_sense.di
 
 import android.content.Context
+import app.cash.sqldelight.db.SqlDriver
+import app.cash.sqldelight.driver.android.AndroidSqliteDriver
 import com.russhwolf.settings.Settings
 import com.russhwolf.settings.SharedPreferencesSettings
 import org.koin.dsl.bind
 import org.koin.dsl.module
+import ru.sbx.spend_sense.db.AppDb
 
 actual val platformModule = module {
     single { get<Context>().getSharedPreferences("appSettings", Context.MODE_PRIVATE) }
     single { SharedPreferencesSettings(get()) } bind Settings::class
+    single<SqlDriver> { AndroidSqliteDriver(AppDb.Schema, get(), "AppDb") }
 }
--- shared/src/jvmMain/kotlin/ru/sbx/spend_sense/di/JvmKoin.kt
+++ shared/src/jvmMain/kotlin/ru/sbx/spend_sense/di/JvmKoin.kt
@@ -1,11 +1,19 @@
 package ru.sbx.spend_sense.di
 
+import app.cash.sqldelight.db.SqlDriver
+import app.cash.sqldelight.driver.jdbc.sqlite.JdbcSqliteDriver
 import com.russhwolf.settings.PreferencesSettings
 import com.russhwolf.settings.Settings
 import org.koin.core.module.Module
 import org.koin.dsl.module
+import ru.sbx.spend_sense.db.AppDb
 import java.util.prefs.Preferences
 
 actual val platformModule: Module = module {
     single<Settings> { PreferencesSettings(Preferences.userRoot()) }
+    single<SqlDriver> {
+        val driver = JdbcSqliteDriver("jdbc:sqlite:AppDb.db")
+        AppDb.Schema.create(driver)
+        driver
+    }
 }
