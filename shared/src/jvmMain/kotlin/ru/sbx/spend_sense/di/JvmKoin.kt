dcdc240be34e12cc196ea0d3d50cf45ab9d791b3
gkuznetsov <kuznetsov.mdev@gmail.com>
2025-07-17 22:38:31 +0500

    SpSe-db: add in-memory DB using for desktop

--- shared/src/jvmMain/kotlin/ru/sbx/spend_sense/di/JvmKoin.kt
+++ shared/src/jvmMain/kotlin/ru/sbx/spend_sense/di/JvmKoin.kt
@@ -12,7 +12,10 @@ import java.util.prefs.Preferences
 actual val platformModule: Module = module {
     single<Settings> { PreferencesSettings(Preferences.userRoot()) }
     single<SqlDriver> {
-        val driver = JdbcSqliteDriver("jdbc:sqlite:AppDb.db")
+        //Todo: need to check if db had been created or not.
+//        val driver = JdbcSqliteDriver("jdbc:sqlite:AppDb.db")
+        //Each app running will create new inmemory DB
+        val driver = JdbcSqliteDriver(JdbcSqliteDriver.IN_MEMORY)
         AppDb.Schema.create(driver)
         driver
     }
