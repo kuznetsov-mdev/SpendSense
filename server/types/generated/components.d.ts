42299625d0acf5ca7b8cce8783b0b64e7c7e9b86
gkuznetsov <kuznetsov.mdev@gmail.com>
2025-07-24 08:43:21 +0500

    SpSe-strapi: prepare content types

--- server/types/generated/components.d.ts
+++ server/types/generated/components.d.ts
@@ -1,3 +1,21 @@
-/*
- * The app doesn't have any components yet.
- */
+import type { Schema, Struct } from '@strapi/strapi';
+
+export interface TimestampsDatetime extends Struct.ComponentSchema {
+  collectionName: 'components_timestamps_datetimes';
+  info: {
+    displayName: 'Datetime';
+    icon: 'clock';
+  };
+  attributes: {
+    createdAtLocal: Schema.Attribute.DateTime;
+    updatedAtLocal: Schema.Attribute.DateTime;
+  };
+}
+
+declare module '@strapi/strapi' {
+  export module Public {
+    export interface ComponentSchemas {
+      'timestamps.datetime': TimestampsDatetime;
+    }
+  }
+}
--- server/types/generated/contentTypes.d.ts
+++ server/types/generated/contentTypes.d.ts
@@ -403,6 +403,7 @@ export interface ApiCategoryCategory extends Struct.CollectionTypeSchema {
     updatedAtLocal: Schema.Attribute.DateTime;
     updatedBy: Schema.Attribute.Relation<'oneToOne', 'admin::user'> &
       Schema.Attribute.Private;
+    userId: Schema.Attribute.String;
   };
 }
 
@@ -438,6 +439,7 @@ export interface ApiSpendEventSpendEvent extends Struct.CollectionTypeSchema {
     updatedAtLocal: Schema.Attribute.DateTime;
     updatedBy: Schema.Attribute.Relation<'oneToOne', 'admin::user'> &
       Schema.Attribute.Private;
+    userId: Schema.Attribute.String;
   };
 }
 
