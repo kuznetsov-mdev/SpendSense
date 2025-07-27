92f73c6ed24b994609d0ce1aa6b48c4f980ba438
gkuznetsov <kuznetsov.mdev@gmail.com>
2025-07-27 16:09:18 +0500

    SpSe-strapi: add custom controller for categories syncing

--- server/src/api/category/controllers/category.ts
+++ server/src/api/category/controllers/category.ts
@@ -4,4 +4,16 @@
 
 import { factories } from '@strapi/strapi'
 
-export default factories.createCoreController('api::category.category');
+export default factories.createCoreController('api::category.category', 
+  ({ strapi }) =>  ({
+    //default
+    async find(ctx) {
+      return null;
+    },
+
+    //custom
+    async sync(ctx) {
+        ctx.body = "data was sync"
+    }
+  })
+);
