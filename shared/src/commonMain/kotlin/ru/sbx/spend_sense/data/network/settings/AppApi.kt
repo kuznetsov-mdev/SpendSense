8e3254762fd4c7a33d63aa961faeae3b83bafaac
gkuznetsov <kuznetsov.mdev@gmail.com>
2025-08-02 15:54:44 +0500

    SpSe-ktor: add network layer for getting category and events

---
+++ shared/src/commonMain/kotlin/ru/sbx/spend_sense/data/network/settings/AppApi.kt
@@ -0,0 +1,45 @@
+package ru.sbx.spend_sense.data.network.settings
+
+import io.ktor.client.HttpClient
+import io.ktor.client.request.bearerAuth
+import io.ktor.client.request.post
+import io.ktor.client.request.setBody
+import io.ktor.http.ContentType
+import io.ktor.http.contentType
+import ru.sbx.spend_sense.data.network.categories.CategoryApi
+import ru.sbx.spend_sense.data.network.events.SpendEventApi
+import ru.sbx.spend_sense.data.network.settings.auth.register.RegisterRequest
+import ru.sbx.spend_sense.data.network.settings.auth.signin.SignInRequest
+import ru.sbx.spend_sense.data.storage.SettingsManager
+
+class AppApi(
+    private val httpClient: HttpClient,
+    private val settings: SettingsManager
+) {
+
+    suspend fun register(req: RegisterRequest) =
+        httpClient.post("${settings.serverUrl}/api/auth/local/register") {
+            contentType(ContentType.Application.Json)
+            setBody(req)
+        }
+
+    suspend fun signIn(req: SignInRequest) =
+        httpClient.post("${settings.serverUrl}/api/auth/local") {
+            contentType(ContentType.Application.Json)
+            setBody(req)
+        }
+
+    suspend fun syncCategories(categories: List<CategoryApi>) =
+        httpClient.post("${settings.serverUrl}/api/categories/sync") {
+            contentType(ContentType.Application.Json)
+            bearerAuth(settings.token)
+            setBody(categories)
+        }
+
+    suspend fun syncEvents(events: List<SpendEventApi>) =
+        httpClient.post("${settings.serverUrl}/api/spend-events/sync") {
+            contentType(ContentType.Application.Json)
+            bearerAuth(settings.token)
+            setBody(events)
+        }
+}
--- shared/src/commonMain/kotlin/ru/sbx/spend_sense/data/storage/SettingsManager.kt
+++ shared/src/commonMain/kotlin/ru/sbx/spend_sense/data/storage/SettingsManager.kt
@@ -7,6 +7,9 @@ import kotlinx.coroutines.flow.update
 
 class SettingsManager(private val settings: Settings) {
     private val THEME_KEY = "THEME_KEY"
+    private val TOKEN_KEY = "TOKEN_KEY"
+    val serverUrl = "http:// 192.168.0.149:1337"
+
 
     private val _isDarkThemeFlow = MutableStateFlow(isDarkTheme)
     val isDarkThemFlow = _isDarkThemeFlow.asStateFlow()
@@ -17,4 +20,8 @@ class SettingsManager(private val settings: Settings) {
             settings.putBoolean(THEME_KEY, value)
         }
         get() = settings.getBoolean(THEME_KEY, true)
+
+    var token: String
+        set(value) = settings.putString(TOKEN_KEY, value)
+        get() = settings.getString(TOKEN_KEY, "")
 }
