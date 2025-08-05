16b2b6fbc95475e389398a4deeb095abd2e91cd1
gkuznetsov <kuznetsov.mdev@gmail.com>
2025-08-05 18:06:50 +0500

    SpSe-ktor: add binding to email and token in SettingsViewModel.kt

--- shared/src/commonMain/kotlin/ru/sbx/spend_sense/presentation/settings/SettingsViewModel.kt
+++ shared/src/commonMain/kotlin/ru/sbx/spend_sense/presentation/settings/SettingsViewModel.kt
@@ -5,26 +5,46 @@ import kotlinx.coroutines.flow.onEach
 import ru.sbx.spend_sense.data.storage.SettingsManager
 import ru.sbx.spend_sense.platform.DeviceInfo
 import ru.sbx.spend_sense.presentation.base.BaseViewModel
-import ru.sbx.spend_sense.presentation.settings.SettingsContract.State
 
 class SettingsViewModel(
-    deviceInfo: DeviceInfo,
+    private val deviceInfo: DeviceInfo,
     private val settingsManager: SettingsManager
-) : BaseViewModel<State, Nothing>() {
+) : BaseViewModel<SettingsContract.State, SettingsContract.Event>() {
 
     init {
-        settingsManager.isDarkThemFlow.onEach {
-            updateState { copy(isDarkTheme = it) }
-        }.launchIn(viewModelScope)
+        bindToEmail()
+        bindToTheme()
+        bindToToken()
+        initDeviceInfo()
+    }
 
+    fun switchTheme(isDark: Boolean) {
+        settingsManager.isDarkTheme = isDark
+    }
+
+    private fun bindToEmail() {
+        settingsManager.emailFlow.onEach { email ->
+            updateState { copy(email = email) }
+        }.launchIn(viewModelScope)
+    }
+
+    private fun bindToToken() {
+        settingsManager.tokenFlow.onEach { token ->
+            updateState { copy(isAuth = token.isNotBlank()) }
+        }.launchIn(viewModelScope)
+    }
+
+    private fun initDeviceInfo() {
         updateState {
             copy(info = deviceInfo.getSummary())
         }
     }
 
-    override fun initialState() = State.NONE
-
-    fun switchTheme(isDark: Boolean) {
-        settingsManager.isDarkTheme = isDark
+    private fun bindToTheme() {
+        settingsManager.isDarkThemFlow.onEach {
+            updateState { copy(isDarkTheme = it) }
+        }.launchIn(viewModelScope)
     }
+
+    override fun initialState() = SettingsContract.State.NONE
 }
