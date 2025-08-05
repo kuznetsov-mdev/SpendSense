0e803f1beb1eed4b6bd1c15de84d86e452909e17
gkuznetsov <kuznetsov.mdev@gmail.com>
2025-08-05 11:14:45 +0500

    SpSe-ktor: add register() method realization

--- shared/src/commonMain/kotlin/ru/sbx/spend_sense/data/storage/SettingsManager.kt
+++ shared/src/commonMain/kotlin/ru/sbx/spend_sense/data/storage/SettingsManager.kt
@@ -8,11 +8,17 @@ import kotlinx.coroutines.flow.update
 class SettingsManager(private val settings: Settings) {
     private val THEME_KEY = "THEME_KEY"
     private val TOKEN_KEY = "TOKEN_KEY"
+    private val EMAIL_KEY = "EMAIL_KEY"
     val serverUrl = "http:// 192.168.0.149:1337"
-
 
     private val _isDarkThemeFlow = MutableStateFlow(isDarkTheme)
     val isDarkThemFlow = _isDarkThemeFlow.asStateFlow()
+
+    private val _tokenFlow = MutableStateFlow(token)
+    val tokenFlow = _tokenFlow.asStateFlow()
+
+    private val _emailFlow = MutableStateFlow(email)
+    val emailFlow = _emailFlow.asStateFlow()
 
     var isDarkTheme: Boolean
         set(value) {
@@ -22,6 +28,16 @@ class SettingsManager(private val settings: Settings) {
         get() = settings.getBoolean(THEME_KEY, true)
 
     var token: String
-        set(value) = settings.putString(TOKEN_KEY, value)
+        set(value) {
+            settings.putString(TOKEN_KEY, value)
+            _tokenFlow.update { value }
+        }
         get() = settings.getString(TOKEN_KEY, "")
+
+    var email: String
+        set(value) {
+            settings.putString(EMAIL_KEY, value)
+            _emailFlow.update { value }
+        }
+        get() = settings.getString(EMAIL_KEY, "")
 }
--- shared/src/commonMain/kotlin/ru/sbx/spend_sense/presentation/auth/register/RegisterViewModel.kt
+++ shared/src/commonMain/kotlin/ru/sbx/spend_sense/presentation/auth/register/RegisterViewModel.kt
@@ -1,18 +1,50 @@
 package ru.sbx.spend_sense.presentation.auth.register
 
+import io.ktor.client.call.body
+import io.ktor.client.statement.bodyAsText
+import io.ktor.http.isSuccess
+import kotlinx.coroutines.CoroutineExceptionHandler
+import kotlinx.coroutines.launch
+import ru.sbx.spend_sense.data.network.ApiErrorWrapper
+import ru.sbx.spend_sense.data.network.AppApi
+import ru.sbx.spend_sense.data.network.auth.AuthResponse
+import ru.sbx.spend_sense.data.network.auth.register.RegisterRequest
+import ru.sbx.spend_sense.data.storage.SettingsManager
+import ru.sbx.spend_sense.extentions.appLog
 import ru.sbx.spend_sense.presentation.base.BaseViewModel
 
-class RegisterViewModel() : BaseViewModel<RegisterContract.State, RegisterContract.Event>() {
+class RegisterViewModel(
+    private val api: AppApi,
+    private val settings: SettingsManager
+) : BaseViewModel<RegisterContract.State, RegisterContract.Event>() {
 
     fun changeEmail(email: String) = updateState { copy(email = email) }
     fun changePassword(pass: String) = updateState { copy(password = pass) }
     fun changeConfirmPassword(pass: String) = updateState { copy(confirmPassword = pass) }
 
     fun register() {
-        //create request model and fill it by registration data
-        //async call
-        //go to server
-        //show result
+        val registerReq = RegisterRequest(
+            email = state.value.email,
+            password = state.value.password,
+            username = state.value.email
+        )
+        viewModelScope.launch(
+            CoroutineExceptionHandler { _, t ->
+                appLog(t.stackTraceToString())
+                pushEvent(RegisterContract.Event.Error(t.message.orEmpty()))
+            }
+        ) {
+            val response = api.register(registerReq)
+            if (response.status.isSuccess()) {
+                val regResponse = response.body<AuthResponse>() //cast response body to generic type -> AuthResponse
+                settings.token = regResponse.jwt.orEmpty()
+                settings.email = state.value.email
+                pushEvent(RegisterContract.Event.Success)
+            } else {
+                val error = response.body<ApiErrorWrapper>().error
+                pushEvent(RegisterContract.Event.Error(error?.message ?: response.bodyAsText()))
+            }
+        }
     }
 
     override fun initialState(): RegisterContract.State = RegisterContract.State.NONE
